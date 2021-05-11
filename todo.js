c183be1d2432ee96bc78e25dece1aa9303d1f6bb
kdbswo <75184363+kdbswo@users.noreply.github.com>
2021-05-11 17:56:42 +0900

    210511 start

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
+
 const TODOS_LS= "toDos";
 
 let toDos = [];
